a8637f86d7d60e225671914800272dc2a8e7ff1f
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-09-29 19:25:11 +0200

    removed answers

--- 002_install_k6/vs.js
+++
@@ -1,6 +0,0 @@
-import http from 'k6/http';
-import { check, group, sleep } from 'k6';
-
-export default function() {
-    let res = http.get('http://bing.com');
-};
--- 005_checks/impl.js
+++ 005_checks/impl.js
@@ -1,8 +1,8 @@
 //k6 use some strange structure - see why 
 //dummy check implementation
-let check = (x, checks) => {
+let check = (value, checks) => {
   for (let c in checks) {
-    console.log(`${c} => ${checks[c].call(this, x)}`);
+    console.log(`${c} => ${checks[c].call(this, value)}`);
   }
 }
 //response
